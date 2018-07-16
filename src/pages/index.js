78bfc263dd3e7a27344e5ea0777957b90f465aac
Anthony Tran <anthonytran.dev@gmail.com>
2018-07-16 20:33:55 +0100

    Testing `deploy-preview` git branch

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import Link from 'gatsby-link'
 const IndexPage = () => (
   <div>
     <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
+    <p>This is an slight update to the old website</p>
     <p>Now go build something great.</p>
     <Link to="/page-2/">Go to page 2</Link>
   </div>
