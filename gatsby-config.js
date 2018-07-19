da4e0fb950a6783ac640844ecbf07b254b6e58ee
Anthony Tran <anthonytran.dev@gmail.com>
2018-07-19 10:09:04 +0100

    Build gatsby-config.js init
    This file was part of the gatsby tutorial and will be used in the final project - the code here will be used for the final project

---
+++ gatsby-config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  siteMetadata: {
+    title: `Blah Blah Fake Title`,
+  },
+  plugins: [
+    `gatsby-plugin-emotion`,
+    `gatsby-transformer-remark`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `src`,
+        path: `${__dirname}/src/`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
+      },
+    },
+  ],
+};
