190abfaa2a5de6b040da63a649a99aa8adfb58d4
Anthony Tran <anthonytran.dev@gmail.com>
2018-07-22 21:12:30 +0100

    Feature /blogs index.js init
    Included the example page layout from the Gatsby.js, as a base to further develop the main blog page

---
+++ src/pages/blogs/index.js
@@ -0,0 +1,53 @@
+import React from "react"
+import { css } from "emotion"
+import Link from "gatsby-link"
+
+const mainHeader = css`
+  display: inline-block;
+  border-bottom: 1px solid;
+`
+
+
+export default ({ data }) => {
+  return (
+    <div>
+      <h1 className={mainHeader}>
+        Amazing Pandas Eating Things
+      </h1>
+      <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
+      {data.allMarkdownRemark.edges.map(({ node }) => (
+        <div key={node.id}>
+          <Link to={node.fields.slug}>
+            <h3 style={{ marginBottom: 10 }}>
+              {node.frontmatter.title}{" "}
+              <span style={{ color: '#BBB' }}>— {node.frontmatter.date}</span>
+            </h3>
+          </Link>
+          <p>{node.excerpt}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export const query = graphql`
+  query BlogPostQuery{
+    allMarkdownRemark( filter: { frontmatter: { type: { eq: "blogs" }}}) {
+      totalCount
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            date(formatString: "DD MMMM, YYYY")
+            type
+          }
+          excerpt
+          fields {
+            slug
+          }
+        }
+      }
+    }
+  }
+`;
