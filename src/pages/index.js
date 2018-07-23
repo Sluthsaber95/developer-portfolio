cebf5ffff1440aa6c76a2d07a6ed5c6afd03c120
Anthony Tran <anthonytran.dev@gmail.com>
2018-07-23 16:19:41 +0100

    Feature Parallax Image Completed
    index.js
    Line 7, float right was used as the image seem all float left, thus this simple correction was utilised.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,26 +1,21 @@
 import React from "react"
+import { Parallax, Background } from 'react-parallax';
 import Link from "gatsby-link"
+import ImageMountainGoat from "./mountain-goat.svg"
+import ImageCloud from "./cloud-parallax.svg"
+const ParallaxLanding = () => (
+  <div style={{float: 'right'}}>
+    <Parallax
+      bgImage={ImageCloud}
+      bgImageAlt="the cat"
+      strength={100}
+    >
+      <img src={ImageMountainGoat} style={{marginTop: 100}}/>
+    </Parallax>
+  </div>
+)
 
 export default () =>
-  <div style={{ margin: '3rem auto', maxWidth: 600 }}>
-    <h1>Richard Hamming on Luck</h1>
-    <div>
-      <p>
-        From Richard Hamming’s classic and must-read talk, “<a href="http://www.cs.virginia.edu/~robins/YouAndYourResearch.html">
-          You and Your Research
-        </a>”.
-      </p>
-      <blockquote>
-        <p>
-          There is indeed an element of luck, and no, there isn’t. The prepared
-          mind sooner or later finds something important and does it. So yes, it
-          is luck.{" "}
-          <em>
-            The particular thing you do is luck, but that you do something is
-            not.
-          </em>
-        </p>
-      </blockquote>
-    </div>
-    <p>Posted April 09, 2011</p>
+  <div style={{ margin: '3rem auto', maxWidth: 1500 , height: 2000}}>
+    <ParallaxLanding />
   </div>
